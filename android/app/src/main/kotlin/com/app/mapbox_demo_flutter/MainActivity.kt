55160feb8aacbf345262bdd9e5c636519bcb3888
Fabio Lin <fabiolin@outlook.com.br>
2023-01-05 13:58:28 -0300

    error in kotlin file

--- android/app/src/main/kotlin/com/app/mapbox_demo_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/mapbox_demo_flutter/MainActivity.kt
@@ -1,33 +1,33 @@
-package com.app.mapbox_demo_flutter
+// package com.app.mapbox_demo_flutter
 
-import android.content.Intent
-import android.os.Bundle
-import com.mapbox.maps.logE
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
+// import android.content.Intent
+// import android.os.Bundle
+// import com.mapbox.maps.logE
+// import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+// class MainActivity : FlutterActivity() {
 
-    private val CHANNEL = "com.app.mapbox_demo_flutter"
+//     private val CHANNEL = "com.app.mapbox_demo_flutter"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+//     override fun onCreate(savedInstanceState: Bundle?) {
+//         super.onCreate(savedInstanceState)
 
 
-        flutterEngine?.dartExecutor?.let {
-            MethodChannel(it.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-                if (call.method == "LatLong") {
-                    val myMMap = HashMap<String, Double>()
-                    myMMap.putAll(call.arguments as HashMap<String, Double>)
-                    val intent = Intent(this, TurnByTurnExperienceActivity::class.java)
-                    intent.putExtra("startPointLat", myMMap["startPointLat"])
-                    intent.putExtra("startPointLon", myMMap["startPointLon"])
-                    intent.putExtra("endPointLat", myMMap["endPointLat"])
-                    intent.putExtra("endPointLong", myMMap["endPointLong"])
-                    startActivity(intent)
-                    result.success("success")
-                }
-            }
-        }
-    }
-}
+//         flutterEngine?.dartExecutor?.let {
+//             MethodChannel(it.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+//                 if (call.method == "LatLong") {
+//                     val myMMap = HashMap<String, Double>()
+//                     myMMap.putAll(call.arguments as HashMap<String, Double>)
+//                     val intent = Intent(this, TurnByTurnExperienceActivity::class.java)
+//                     intent.putExtra("startPointLat", myMMap["startPointLat"])
+//                     intent.putExtra("startPointLon", myMMap["startPointLon"])
+//                     intent.putExtra("endPointLat", myMMap["endPointLat"])
+//                     intent.putExtra("endPointLong", myMMap["endPointLong"])
+//                     startActivity(intent)
+//                     result.success("success")
+//                 }
+//             }
+//         }
+//     }
+// }
